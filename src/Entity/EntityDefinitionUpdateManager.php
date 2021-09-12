9f21355b254f35fed48802c093f140a75e6c76e1
Rajab Natshah <rajabn@gmail.com>
2021-09-12 21:48:40 +0300

    Init Entity Definition Update Manager #1

--- src/Entity/EntityDefinitionUpdateManager.php
+++ src/Entity/EntityDefinitionUpdateManager.php
@@ -2,16 +2,9 @@
 
 namespace Vardot\Entity;
 
-use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface;
-use Drupal\Core\Entity\EntityFieldManagerInterface;
-use Drupal\Core\Entity\EntityLastInstalledSchemaRepositoryInterface;
-use Drupal\Core\Entity\EntityTypeListenerInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\Schema\EntityStorageSchemaInterface;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
-use Drupal\Core\Field\FieldStorageDefinitionListenerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Entity Definition Update Manager.
@@ -20,96 +13,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * https://www.drupal.org/project/devel_entity_updates
  * Development version of the entity definition update manager.
  */
-class EntityDefinitionUpdateManager implements ContainerInjectionInterface {
-
-  /**
-   * The entity definition update manager.
-   *
-   * @var \Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface
-   */
-  private $entityDefinitionUpdateManager;
-
-  /**
-   * The last installed schema repository.
-   *
-   * @var \Drupal\Core\Entity\EntityLastInstalledSchemaRepositoryInterface
-   */
-  private $entityLastInstalledSchemaRepository;
-
-  /**
-   * The entity type manager service.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  private $entityTypeManager;
-
-  /**
-   * The entity type listener service.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeListenerInterface
-   */
-  private $entityTypeListener;
-
-  /**
-   * The entity field manager service.
-   *
-   * @var \Drupal\Core\Entity\EntityFieldManagerInterface
-   */
-  private $entityFieldManager;
-
-  /**
-   * The field storage definition listener service.
-   *
-   * @var \Drupal\Core\Field\FieldStorageDefinitionListenerInterface
-   */
-  private $fieldStorageDefinitionListener;
-
-  /**
-   * Constructs a new EntityDefinitionUpdateManager.
-   *
-   * @param \Drupal\Core\Entity\EntityDefinitionUpdateManagerInterface $entity_definition_update_manager
-   *   The entity definition update manager.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager service.
-   * @param \Drupal\Core\Entity\EntityLastInstalledSchemaRepositoryInterface $entity_last_installed_schema_repository
-   *   The last installed schema repository service.
-   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
-   *   The entity field manager service.
-   * @param \Drupal\Core\Entity\EntityTypeListenerInterface $entity_type_listener
-   *   The entity type listener interface.
-   * @param \Drupal\Core\Field\FieldStorageDefinitionListenerInterface $field_storage_definition_listener
-   *   The field storage definition listener service.
-   */
-  public function __construct(
-    EntityDefinitionUpdateManagerInterface $entity_definition_update_manager,
-    EntityTypeManagerInterface $entity_type_manager,
-    EntityLastInstalledSchemaRepositoryInterface $entity_last_installed_schema_repository,
-    EntityFieldManagerInterface $entity_field_manager,
-    EntityTypeListenerInterface $entity_type_listener,
-    FieldStorageDefinitionListenerInterface $field_storage_definition_listener
-  ) {
-    $this->entityDefinitionUpdateManager = $entity_definition_update_manager;
-    $this->entityTypeManager = $entity_type_manager;
-    $this->entityLastInstalledSchemaRepository = $entity_last_installed_schema_repository;
-    $this->entityFieldManager = $entity_field_manager;
-    $this->entityTypeListener = $entity_type_listener;
-    $this->fieldStorageDefinitionListener = $field_storage_definition_listener;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    /** @noinspection PhpParamsInspection */
-    return new static(
-      $container->get('entity.definition_update_manager'),
-      $container->get('entity_type.manager'),
-      $container->get('entity.last_installed_schema.repository'),
-      $container->get('entity_field.manager'),
-      $container->get('entity_type.listener'),
-      $container->get('field_storage_definition.listener')
-    );
-  }
+class EntityDefinitionUpdateManager {
 
   /**
    * Applies all the detected valid changes.
@@ -120,8 +24,8 @@ class EntityDefinitionUpdateManager implements ContainerInjectionInterface {
 
     if ($complete_change_list) {
       // In case there are changes, explicitly invalidate caches.
-      $this->entityTypeManager->clearCachedDefinitions();
-      $this->entityFieldManager->clearCachedFieldDefinitions();
+      \Drupal::entityTypeManager()->clearCachedDefinitions();
+      \Drupal::service('entity_field.manager')->clearCachedFieldDefinitions();
     }
 
     foreach ($complete_change_list as $entity_type_id => $change_list) {
@@ -135,8 +39,8 @@ class EntityDefinitionUpdateManager implements ContainerInjectionInterface {
 
       // Process field storage definition changes.
       if (!empty($change_list['field_storage_definitions'])) {
-        $storage_definitions = $this->entityFieldManager->getFieldStorageDefinitions($entity_type_id);
-        $original_storage_definitions = $this->entityLastInstalledSchemaRepository->getLastInstalledFieldStorageDefinitions($entity_type_id);
+        $storage_definitions = \Drupal::service('entity_field.manager')->getFieldStorageDefinitions($entity_type_id);
+        $original_storage_definitions = \Drupal::service('entity.last_installed_schema.repository')->getLastInstalledFieldStorageDefinitions($entity_type_id);
 
         foreach ($change_list['field_storage_definitions'] as $field_name => $change) {
           $storage_definition = isset($storage_definitions[$field_name]) ? $storage_definitions[$field_name] : NULL;
@@ -157,21 +61,21 @@ class EntityDefinitionUpdateManager implements ContainerInjectionInterface {
    *   The entity type ID.
    */
   private function doEntityUpdate($op, $entity_type_id) {
-    $entity_type = $this->entityTypeManager->getDefinition($entity_type_id);
+    $entity_type = \Drupal::entityTypeManager()->getDefinition($entity_type_id);
     switch ($op) {
       case EntityDefinitionUpdateManagerInterface::DEFINITION_CREATED:
-        $this->entityTypeListener->onEntityTypeCreate($entity_type);
+        \Drupal::service('entity_type.listener')->onEntityTypeCreate($entity_type);
         break;
 
       case EntityDefinitionUpdateManagerInterface::DEFINITION_UPDATED:
-        $original = $this->entityLastInstalledSchemaRepository->getLastInstalledDefinition($entity_type_id);
-        $storage = $this->entityTypeManager->getStorage($entity_type->id());
+        $original = \Drupal::service('entity.last_installed_schema.repository')->getLastInstalledDefinition($entity_type_id);
+        $storage = \Drupal::entityTypeManager()->getStorage($entity_type->id());
         if ($storage instanceof EntityStorageSchemaInterface && $storage->requiresEntityDataMigration($entity_type, $original)) {
           throw new \InvalidArgumentException('The entity schema update for the ' . $entity_type->id() . ' entity type requires a data migration.');
         }
-        $field_storage_definitions = $this->entityFieldManager->getFieldStorageDefinitions($entity_type_id);
-        $original_field_Storage_definitions = $this->entityLastInstalledSchemaRepository->getLastInstalledFieldStorageDefinitions($entity_type_id);
-        $this->entityTypeListener->onFieldableEntityTypeUpdate($entity_type, $original, $field_storage_definitions, $original_field_Storage_definitions);
+        $field_storage_definitions = \Drupal::service('entity.last_installed_schema.repository')->getFieldStorageDefinitions($entity_type_id);
+        $original_field_Storage_definitions = \Drupal::service('entity.last_installed_schema.repository')->getLastInstalledFieldStorageDefinitions($entity_type_id);
+        \Drupal::service('entity_type.listener')->onFieldableEntityTypeUpdate($entity_type, $original, $field_storage_definitions, $original_field_Storage_definitions);
         break;
     }
   }
@@ -192,18 +96,18 @@ class EntityDefinitionUpdateManager implements ContainerInjectionInterface {
   private function doFieldUpdate($op, FieldStorageDefinitionInterface $storage_definition = NULL, FieldStorageDefinitionInterface $original_storage_definition = NULL) {
     switch ($op) {
       case EntityDefinitionUpdateManagerInterface::DEFINITION_CREATED:
-        $this->fieldStorageDefinitionListener->onFieldStorageDefinitionCreate($storage_definition);
+        \Drupal::service('field_storage_definition.listener')->onFieldStorageDefinitionCreate($storage_definition);
         break;
 
       case EntityDefinitionUpdateManagerInterface::DEFINITION_UPDATED:
         if ($storage_definition && $original_storage_definition) {
-          $this->fieldStorageDefinitionListener->onFieldStorageDefinitionUpdate($storage_definition, $original_storage_definition);
+          \Drupal::service('field_storage_definition.listener')->onFieldStorageDefinitionUpdate($storage_definition, $original_storage_definition);
         }
         break;
 
       case EntityDefinitionUpdateManagerInterface::DEFINITION_DELETED:
         if ($original_storage_definition) {
-          $this->fieldStorageDefinitionListener->onFieldStorageDefinitionDelete($original_storage_definition);
+          \Drupal::service('field_storage_definition.listener')->onFieldStorageDefinitionDelete($original_storage_definition);
         }
         break;
     }
